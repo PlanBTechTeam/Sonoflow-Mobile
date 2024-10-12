e0ea6f460a907e58c49e0b74963bb4396f80abd3
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-11 22:13:05 -0300

    feat(main): Coloquei como home o AuthProvider para testar a navegação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sonoflow/app/view/auth_screen.dart';
+import 'package:sonoflow/presentation/provider/auth_provider.dart';
+import 'package:sonoflow/presentation/view/auth_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AuthScreen(),
+      home: const AuthProvider(),
     );
   }
 }
