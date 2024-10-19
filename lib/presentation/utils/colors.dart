450b3052dee0ac163e669a4719da1536bfc3d046
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-19 03:57:18 -0300

    update(color): 1 Cor nova

--- lib/presentation/utils/colors.dart
+++ lib/presentation/utils/colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   static const Color lemonYellow = Color.fromRGBO(240, 208, 51, 1);
   static const Color midnightBlue = Color.fromRGBO(15, 27, 64, 1);
   static const Color goldenYellow = Color.fromRGBO(196, 162, 90, 1);
+  static const Color ghostWhite = Color.fromRGBO(245, 245, 253, 1);
   static const LinearGradient linearBlueGradient = LinearGradient(
     colors: [
       Color.fromRGBO(16, 28, 66, 1),
