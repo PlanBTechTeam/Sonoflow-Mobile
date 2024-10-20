5c3b7e2871c1a27fb350c76b32693fb49a98ee59
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-20 14:43:55 -0300

    feat(main): Adicionei um 'provider' (ChangeLayerViewModel) ao 'Main'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sonoflow/firebase_options.dart';
 import 'package:sonoflow/presentation/provider/auth_provider.dart';
+import 'package:sonoflow/presentation/viewmodel/change_layer_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +11,12 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ChangeLayerViewmodel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
