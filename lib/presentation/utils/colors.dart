27f44cf26cec8330a8e69baeba338b09e5d95c47
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-20 14:45:10 -0300

    feat(colors): Adicionei cores

--- lib/presentation/utils/colors.dart
+++ lib/presentation/utils/colors.dart
@@ -5,10 +5,10 @@ import 'package:flutter/material.dart';
 /// Esta classe contém todas as cores personalizadas usadas no aplicativo.
 class AppColors {
   static const Color mediumGray = Color.fromRGBO(161, 161, 161, 1);
-  static const Color lemonYellow = Color.fromRGBO(240, 208, 51, 1);
   static const Color midnightBlue = Color.fromRGBO(15, 27, 64, 1);
   static const Color goldenYellow = Color.fromRGBO(196, 162, 90, 1);
   static const Color ghostWhite = Color.fromRGBO(245, 245, 253, 1);
+  static const Color steelBlue = Color.fromRGBO(94, 105, 128, 0.2);
   static const LinearGradient linearBlueGradient = LinearGradient(
     colors: [
       Color.fromRGBO(16, 28, 66, 1),
@@ -17,4 +17,5 @@ class AppColors {
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
   );
+  static const Color softSkyBlueGradient = Color.fromRGBO(188, 231, 255, 0.51);
 }
