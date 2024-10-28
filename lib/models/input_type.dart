aa14f35485009f71caa8a9b5ff841d7b708ee824
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-28 20:42:22 -0300

    feat(InputType): Criei um "model" par os tipos de Input

---
+++ lib/models/input_type.dart
@@ -0,0 +1,17 @@
+/// Enumeração que define os tipos de entrada (input) possíveis
+/// no formulário ou componentes da interface do usuário.
+/// - `date`: Representa uma entrada do tipo data.
+/// - `time`: Representa uma entrada do tipo hora.
+/// - `text`: Representa uma entrada do tipo texto.
+enum InputType {
+  date,
+  time,
+  text,
+  select,
+  count,
+  slider,
+  choiceChip,
+  bool
+}
+
+
