ff18a9624c0a7acee0871117395e47f3db229b3d
Kaue Miziara <kauemiziara@gmail.com>
2025-01-21 16:50:32 -0300

    feat(view): Adiciona a opção de tirar uma foto nova ou escolher da galeria ao clicar no widget `PhotoInput`

--- lib/presentation/view/components/photo_input.dart
+++ lib/presentation/view/components/photo_input.dart
@@ -15,22 +15,12 @@ class PhotoInput extends StatelessWidget {
     required this.onImagePicked,
   });
 
-  Future<void> _pickImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    if (pickedFile != null) {
-      final imageFile = File(pickedFile.path);
-      onImagePicked(imageFile);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GestureDetector(
-          onTap: _pickImage,
+          onTap: () => _showSourceSelectionDialog(context),
           child: Container(
             decoration: BoxDecoration(
               color: Colors.grey[300],
@@ -51,5 +41,45 @@ class PhotoInput extends StatelessWidget {
         const SizedBox(height: 16),
       ],
     );
+  }
+
+  void _showSourceSelectionDialog(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.camera),
+                title: const Text('Tire uma foto'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.camera);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Escolha da Galeria'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.gallery);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      final imageFile = File(pickedFile.path);
+      onImagePicked(imageFile);
+    }
   }
 }
