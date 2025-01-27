a3f21ce04f5c49fd50692c23807fe6a15ec70c74
Kaue Miziara <kauemiziara@gmail.com>
2025-01-26 22:11:56 -0300

    refactor(view): Altera o widget `WelcomeWidget` para renderizar imagens a partir de uma URL

--- lib/presentation/view/components/welcome_widget.dart
+++ lib/presentation/view/components/welcome_widget.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:sonoflow/presentation/utils/colors.dart';
 
-/**
- * WELCOME WIDGET
- *
- * Um widget que exibe uma mensagem de boas-vindas personalizada ao usuário.
- *
- * Este widget possui dois parâmetros:
- * - [username]: O nome do usuário que será exibido na mensagem de boas-vindas.
- * - [photoURL]: O URL da imagem do usuário que será exibida como foto de perfil.
- */
+/// WELCOME WIDGET
+///
+/// Um widget que exibe uma mensagem de boas-vindas personalizada ao usuário.
+///
+/// Este widget possui dois parâmetros:
+/// - [username]: O nome do usuário que será exibido na mensagem de boas-vindas.
+/// - [photoURL]: O URL da imagem do usuário que será exibida como foto de perfil.
 class WelcomeWidget extends StatelessWidget {
   final String username; // Nome do usuário
   final String photoURL; // URL da imagem do usuário
@@ -31,9 +29,7 @@ class WelcomeWidget extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
           ),
           clipBehavior: Clip.antiAlias,
-          child: Image(
-            image: AssetImage(photoURL),
-          ),
+          child: Image.network(photoURL),
         ),
         const SizedBox(width: 10), // Espaçamento entre a imagem e o texto
         Column(
