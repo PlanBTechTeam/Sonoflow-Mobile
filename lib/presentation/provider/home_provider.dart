c6ff61b5e9d0528262f4e2a364c5ceee681b2a5d
Kaue Miziara <kauemiziara@gmail.com>
2025-01-26 22:51:50 -0300

    change(view): Desabilita o botão de Dashboard em `HomeProvider` temporariamente

--- lib/presentation/provider/home_provider.dart
+++ lib/presentation/provider/home_provider.dart
@@ -44,14 +44,24 @@ class _HomeProviderState extends State<HomeProvider> {
         currentIndex: _currentIndex,
         onTap: (int newIndex) {
           setState(() {
-            _currentIndex = newIndex;
+            // TODO: remover checagem para habilitar o botão
+            if (newIndex != 0) {
+              _currentIndex = newIndex;
+            }
           });
         },
         items: <BottomNavigationBarItem>[
           // ===== DASHBOARD ICON =====
           BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/dashboard.png'),
-            activeIcon: Image.asset('assets/icons/dashboard-active.png'),
+            // TODO: remover opacidade para habilitar botão
+            icon: Opacity(
+              opacity: 0.25,
+              child: Image.asset('assets/icons/dashboard.png'),
+            ),
+            activeIcon: Opacity(
+              opacity: 0.25,
+              child: Image.asset('assets/icons/dashboard-active.png'),
+            ),
             label: 'Dashboard',
           ),
 
